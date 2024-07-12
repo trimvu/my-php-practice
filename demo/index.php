93197ffe462b0c80162ba8349039097fb290cc00
Tri <vutriminh@gmail.com>
2024-07-12 15:27:38 -0400

    worked on associative arrays and looping through them

--- demo/index.php
+++ demo/index.php
@@ -8,6 +8,7 @@
 
     <h1>Recommende Books</h1>
 
+    <!-- arrays -->
     <?php 
         $books = [
             "Do Androids Dream of Electric Sheep",
@@ -16,6 +17,25 @@
         ];
     ?>
 
+    <!-- associative arrays -->
+    <?php 
+        $booksAssociativeArray = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'published' => 1968,
+                'author' => 'Philip K. Dick',
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'published' => 2021,
+                'author' => 'Andy Weir',
+                'purchaseUrl' => 'http://example.com'
+            ]
+        ]
+    ?>
+
+    <!-- going through a foreach loop of an array -->
     <ul>
         <?php 
             foreach ($books as $book) {
@@ -24,10 +44,27 @@
         ?>
     </ul>
 
+    <!-- another (cleaner) way of going through a foreach loop of an array -->
     <ul>
         <?php foreach ($books as $book) : ?>
             <li><?= $book ?></li>
         <?php endforeach; ?>
+    </ul>
+
+    <!-- Giving the index 1 -->
+    <p>
+        <?= $books[1] ?>
+    </p>
+
+    <!-- going through a foreach loop of an associative array -->
+    <ul>
+        <?php foreach ($booksAssociativeArray as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl']; ?>">
+                    <?= $book['name'] . ' - ' . $book['published']; ?>
+                </a>
+            </li>
+        <?php endforeach ?>
     </ul>
 
 </body>
