bbd3b29bb4920d4c4f9ca7457cd00380a883b919
Tri <vutriminh@gmail.com>
2024-07-12 15:22:43 -0400

    php demo and readme file

---
+++ demo/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+
+    <h1>
+        <?php 
+            echo "Hello, World";
+        ?>
+    </h1>
+
+    <h2>
+        <?php 
+            echo "Hello, Country";
+        ?>
+    </h2>
+
+    <p>
+        <?php 
+            echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis veniam enim quisquam vel laborum quibusdam voluptate repellat cupiditate quia asperiores iure neque voluptas ipsum libero, quaerat unde corporis? Consectetur, neque!";
+        ?>
+    </p>
+
+</body>
+</html>
