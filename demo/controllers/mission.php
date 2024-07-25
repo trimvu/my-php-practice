f8b3fc7a6be9de4d14bf2f36fb826e55648582f7
Tri <vutriminh@gmail.com>
2024-07-24 23:46:10 -0400

    creating php router

--- demo/controllers/mission.php
+++ demo/controllers/mission.php
@@ -1,7 +1,5 @@
 <?php 
 
-require 'functions.php';
-
 $heading = 'Our Mission';
 
 require "views/mission.view.php";
--- demo/views/partials/nav.php
+++ demo/views/partials/nav.php
@@ -9,9 +9,9 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               <a href="/" class="<?= urlIs('/') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white' ?>" aria-current="page">Home</a>
-              <a href="/about.php" class="<?= urlIs('/about.php') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white' ?>">About</a>
-              <a href="/contact.php" class="<?= urlIs('/contact.php') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white' ?>">Contact</a>
-              <a href="/mission.php" class="<?= urlIs('/mission.php') === '/mission.php' ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white' ?>">Our Mission</a>
+              <a href="/about" class="<?= urlIs('/about') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white' ?>">About</a>
+              <a href="/contact" class="<?= urlIs('/contact') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white' ?>">Contact</a>
+              <a href="/mission" class="<?= urlIs('/mission') ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white' ?>">Our Mission</a>
             </div>
           </div>
         </div>
