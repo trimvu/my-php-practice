d5f56dd5982038ac88f4b4e8c11644dddd6e0c57
Tri <vutriminh@gmail.com>
2024-07-27 19:34:08 -0400

    environments and configuration flexibility

--- demo/Database.php
+++ demo/Database.php
@@ -5,12 +5,16 @@ class Database
 {
     public $connection;
 
-    public function __construct()
+    public function __construct($config, $username = 'root', $password = '')
     {
-        // $dsn = data source name
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+        $dsn = 'mysql:' . http_build_query($config, '', ';'); // example.com?host=localhost&port=3306&dbname=myapp
 
-        $this->connection = new PDO($dsn);
+        // $dsn = data source name
+        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
     public function query($query)
     {
--- demo/index.php
+++ demo/index.php
@@ -6,8 +6,10 @@ require 'functions.php';
 
 require 'Database.php';
 
-$db = new Database();
+$config = require('config.php');
 
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+
+$posts = $db->query("select * from posts")->fetchAll();
 
 dd($posts);
