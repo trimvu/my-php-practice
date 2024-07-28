934d10d533bc3197f8c6f9e91777e82c1bcfe308
Tri <vutriminh@gmail.com>
2024-07-27 20:09:25 -0400

    refactored sql query

--- demo/config.php
+++ demo/config.php
@@ -3,8 +3,8 @@
 return [
     'database' => [
         'host' => 'localhost',
-    'port' => 3306,
-    'dbname' => 'myapp',
-    'charset' => 'utf8mb4'
+        'port' => 3306,
+        'dbname' => 'myapp',
+        'charset' => 'utf8mb4'
     ]
 ];
--- demo/index.php
+++ demo/index.php
@@ -10,6 +10,11 @@ $config = require('config.php');
 
 $db = new Database($config['database']);
 
-$posts = $db->query("select * from posts")->fetchAll();
+$id = $_GET['id'];
+// $query = "select * from posts where id = ?";
+$query = "select * from posts where id = :id";
+
+// $posts = $db->query($query, [$id])->fetch();
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
